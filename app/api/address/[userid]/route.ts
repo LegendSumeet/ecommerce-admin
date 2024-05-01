99d844cd852c17e2796ab62b722771d90575af95
LegendSumeet <sumeetgithub@gmail.com>
2024-05-01 19:55:30 +0530

    GFGFGF

--- app/api/address/[userid]/route.ts
+++ app/api/address/[userid]/route.ts
@@ -4,30 +4,28 @@ import { connectToDB } from "@/lib/mongoDB";
 import ShippingAddress from "@/lib/models/address";
 
 export const GET = async (
-    req: NextRequest,
-    { params }: { params: { userid: string } }
-  ) => {
-    try {
-      await connectToDB();
-      const addresses = await ShippingAddress.find({userid:params.userid});
-      if (!addresses) {
-        return new NextResponse(
-          JSON.stringify({ message: "address not found" }),
-          { status: 404 },
-        
-        );
-      }
-  
-      return new NextResponse(JSON.stringify(addresses),{
-        headers: {
-          "Access-Control-Allow-Origin": "http://localhost:3001",
-          "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-          "Access-Control-Allow-Headers": "Content-Type",
-        },
-      });
-    } catch (err) {
-      console.log("[collectionId_GET]", err);
-      return new NextResponse("Internal error", { status: 500 });
+  req: NextRequest,
+  { params }: { params: { userid: string } }
+) => {
+  try {
+    await connectToDB();
+    const addresses = await ShippingAddress.find({ userid: params.userid });
+    if (!addresses || addresses.length === 0) {
+      return new NextResponse(
+        JSON.stringify({ message: "Addresses not found for the user" }),
+        { status: 404 }
+      );
     }
-  };
-  
+
+    return new NextResponse(JSON.stringify(addresses), {
+      headers: {
+        "Access-Control-Allow-Origin": "http://localhost:3001",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type",
+      },
+    });
+  } catch (err) {
+    console.error("[collectionId_GET]", err);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+};
