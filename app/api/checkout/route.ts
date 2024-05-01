804875c8cf127c22819352c7690a82b3f8013a09
LegendSumeet <sumeetgithub@gmail.com>
2024-05-01 15:44:42 +0530

    updated

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -1,7 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import { stripe } from "@/lib/stripe";
-const Razorpay = require("razorpay");
-const shortid = require("shortid");
+import Razorpay from "razorpay";
+import { randomInt } from "crypto";
+
 
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
@@ -20,33 +21,47 @@ export async function POST(req: NextRequest) {
     if (!cartItems || !customer) {
       return new NextResponse("Not enough data to checkout", { status: 400 });
     }
+    
+    if (!process.env.RAZORPAY_KEY) {
+      return new NextResponse("Razorpay key is not defined", { status: 500 });
+    }
+    
+    const razorpayKey = process.env.RAZORPAY_KEY;
+    let totalPrice = 0;
+
+    // Loop through each item in the cart and calculate total price
+    cartItems.forEach((cartItem: { item: { price: number; }; quantity: number; }) => {
+        totalPrice += cartItem.item.price * cartItem.quantity;
+    });
     const razorpay = new Razorpay({
-      key_id: process.env.RAZORPAY_KEY,
+      key_id: razorpayKey,
       key_secret: process.env.RAZORPAY_SECRET,
     });
     const payment_capture = 1;
-    const amount = cartItems.item.price;
     const currency = "INR";
     const options = {
-      amount: (amount * 100).toString(),
+      amount: totalPrice,
       currency,
-      receipt: shortid.generate(),
+      receipt: randomInt(100000, 999999).toString(),
       payment_capture,
     };
 
     try {
+      // Create Razorpay order
       const response = await razorpay.orders.create(options);
-      NextResponse.json({
+
+      // Respond with order details
+      return new NextResponse(JSON.stringify({
         id: response.id,
         currency: response.currency,
         amount: response.amount,
-      });
+      }));
     } catch (err) {
-      console.log(err);
-      NextResponse.json(err);
+      console.error("Error creating Razorpay order:", err);
+      return new NextResponse("Error creating Razorpay order", { status: 500 });
     }
   } catch (err) {
-    console.log("[checkout_POST]", err);
+    console.error("[checkout_POST]", err);
     return new NextResponse("Internal Server Error", { status: 500 });
   }
 }
