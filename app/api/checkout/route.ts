626dab4f5634830e09e94c3448daaeb2b86db865
LegendSumeet <sumeetgithub@gmail.com>
2024-05-01 15:53:42 +0530

    fdsfdsf

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -4,14 +4,10 @@ import Razorpay from "razorpay";
 import { randomInt } from "crypto";
 
 
-const corsHeaders = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type, Authorization",
-};
+
 
 export async function OPTIONS() {
-  return NextResponse.json({}, { headers: corsHeaders });
+  return NextResponse.json({});
 }
 
 export async function POST(req: NextRequest) {
